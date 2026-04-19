eb01fd1b85459a5d06186a8a415b59cea66b96b4
Jharwal77 <rahuljharwal2468@gmail.com>
2026-04-19 16:34:40 +0530

    here i edit env

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Change this to your backend URL
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
 
 const api = axios.create({
   baseURL: BASE_URL,
