c574c55018545f026719f042a991ed80e54d9035
Jharwal77 <rahuljharwal2468@gmail.com>
2026-04-19 17:35:34 +0530

    fix: hardcode backend URL for production

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Change this to your backend URL
-const BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
+const BASE_URL = "https://mainbackend-u3m9.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
